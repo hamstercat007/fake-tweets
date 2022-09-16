3b143296af5aeb168c63d1752f4825191d7e40d6
Lorentz Bloom <lorentzbloomguitar@gmail.com>
2022-09-16 14:41:28 +0100

    checked items are rendering

--- src/Components/TweetCardList.js
+++ src/Components/TweetCardList.js
@@ -1,18 +1,31 @@
 import TweetCard from './TweetCard.js';
+import {tweetData} from "../tweetData.js"
+
 function TweetCardList() {
     return (
-        <div
-            style={{
-                backgroundColor: 'lightgreen',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-            }}
-        >
-            TweetCardList
-            {/* Map array of objects and creating element per object */}
-            <TweetCard />
-        </div>
+      <div
+        style={{
+          backgroundColor: "lightgreen",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        TweetCardList
+        {/* Map array of objects and creating element per object */}
+        {tweetData.map((tweet) => {
+          return (
+            <TweetCard
+              key={tweet.id}
+              userName={tweet.userName}
+              timeStamp={tweet.timeStamp}
+              comments={tweet.comments}
+              content={tweet.content}
+            />
+          );
+        })}
+        <TweetCard />
+      </div>
     );
 }
 
---
+++ src/tweetData.js
@@ -0,0 +1,66 @@
+export const tweetData = [
+  {
+    id: 1,
+    userName: "Mike",
+    timeStamp: "16/09/2022",
+    content:
+      "A tree needs to be your friend if you're going to paint him. You can create beautiful things - but you have to see them in your mind first. So often we avoid running water, and running water is a lot of fun.",
+    comments: [],
+  },
+  {
+    id: 2,
+    userName: "Sam",
+    timeStamp: "16/09/2022",
+    content:
+      "A tree needs to be your friend if you're going to paint him. You can create beautiful things - but you have to see them in your mind first. So often we avoid running water, and running water is a lot of fun.",
+    comments: [],
+  },
+  {
+    id: 3,
+    userName: "Lisa",
+    timeStamp: "16/09/2022",
+    content:
+      "A tree needs to be your friend if you're going to paint him. You can create beautiful things - but you have to see them in your mind first. So often we avoid running water, and running water is a lot of fun.",
+    comments: ["Green power ftw!"],
+  },
+  {
+    id: 4,
+    userName: "Lorentz",
+    timeStamp: "16/09/2022",
+    content:
+      "A tree needs to be your friend if you're going to paint him. You can create beautiful things - but you have to see them in your mind first. So often we avoid running water, and running water is a lot of fun.",
+    comments: [],
+  },
+  {
+    id: 5,
+    userName: "Mikey",
+    timeStamp: "16/09/2022",
+    content:
+      "A tree needs to be your friend if you're going to paint him. You can create beautiful things - but you have to see them in your mind first. So often we avoid running water, and running water is a lot of fun.",
+    comments: [],
+  },
+  {
+    id: 6,
+    userName: "Sammy",
+    timeStamp: "16/09/2022",
+    content:
+      "A tree needs to be your friend if you're going to paint him. You can create beautiful things - but you have to see them in your mind first. So often we avoid running water, and running water is a lot of fun.",
+    comments: [],
+  },
+  {
+    id: 7,
+    userName: "Lisah",
+    timeStamp: "16/09/2022",
+    content:
+      "A tree needs to be your friend if you're going to paint him. You can create beautiful things - but you have to see them in your mind first. So often we avoid running water, and running water is a lot of fun.",
+    comments: [],
+  },
+  {
+    id: 8,
+    userName: "Lozzy",
+    timeStamp: "16/09/2022",
+    content:
+      "A tree needs to be your friend if you're going to paint him. You can create beautiful things - but you have to see them in your mind first. So often we avoid running water, and running water is a lot of fun.",
+    comments: [],
+  },
+];
